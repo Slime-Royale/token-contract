e22347f45e785a9160de3f0c2e12973cbbcae096
Dung Nguyen <vietdungvn88@gmail.com>
2022-07-04 17:52:24 +0700

    Hard cap SCE

--- SCE.sol
+++ SCE.sol
@@ -2,53 +2,41 @@
 pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/access/AccessControl.sol";
-import "@openzeppelin/contracts/utils/cryptography/ECDSA.sol";
-import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "./extensions/BPContract.sol";
 
-contract SCE is AccessControl, ERC20Burnable {
+contract SCE is ERC20, AccessControl {
     bytes32 public constant OWNER_ROLE = keccak256("OWNER_ROLE");
-    bytes32 public constant SIGNER_ROLE = keccak256("SIGNER_ROLE");
+    bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE");
 
-    using ECDSA for bytes32;
-    mapping(string => uint) public claimed;
+    uint public MAX_SUPPLY = 25_000_000_000 * (10 ** decimals());
+    uint public DAILY_MINT = 12_500_000 * (10 ** decimals());
+    address public CLAIM_POOL;
 
     bool public isInPreventBotMode;
     BPContract public BP;
 
-    event BurnForReason(uint amount, string reason);
-    event Claim(address indexed user, string itx, uint amount);
-
     constructor() ERC20("Slime Royale Cupid Essence", "SCE") {
 
-        _setRoleAdmin(SIGNER_ROLE, OWNER_ROLE);
+        _setRoleAdmin(MINTER_ROLE, OWNER_ROLE);
         _setRoleAdmin(OWNER_ROLE, OWNER_ROLE);
         _setupRole(OWNER_ROLE, msg.sender);
+
+        //Pre-mint for LP & play-to-earn early rewards
+        _mint(msg.sender, 100_000_000 * (10 ** decimals()));
     }
 
-    function burnFor(uint amount, string calldata reason) public {
+    function burn(uint amount) public {
+        MAX_SUPPLY -= amount;
         _burn(msg.sender, amount);
-        emit BurnForReason(amount, reason);
     }
 
-    function claim(address user, string calldata itx, uint amount, uint expire, bytes memory signature) public {
-        require(amount > 0, "invalid amount");
-        require(claimed[itx] == 0, "itx was claimed");
-        require(verifySignature(user, itx, amount, expire, signature), "wrong signature");
-        claimed[itx] = amount;
-
-        _mint(user, amount);
-
-        emit Claim(user, itx, amount);
-    }
-
-    function verifySignature(address user, string calldata itx, uint amount, uint expire, bytes memory signature)
-    internal view returns (bool) {
-        require(block.timestamp < expire, "signature expire");
-        bytes32 hash = keccak256(abi.encodePacked(user, itx, amount, expire, address(this)));
-        bytes32 messageHash = hash.toEthSignedMessageHash();
-        address signatory = messageHash.recover(signature);
-        return hasRole(SIGNER_ROLE, signatory);
+    function mint() external onlyRole(MINTER_ROLE) {
+        require(CLAIM_POOL != address(0), "SCE:: must be config claim pool");
+        uint supplyAfterMint = totalSupply() + DAILY_MINT;
+        uint amount = supplyAfterMint > MAX_SUPPLY ? MAX_SUPPLY - totalSupply() : DAILY_MINT;
+        require(amount > 0, "SCE:: max minted");
+        _mint(CLAIM_POOL, amount);
     }
 
     function togglePreventBotMode() public onlyRole(OWNER_ROLE) {
@@ -56,8 +44,18 @@ contract SCE is AccessControl, ERC20Burnable {
     }
 
     function setBPContract(address _bp) public onlyRole(OWNER_ROLE) {
-        require(_bp == address(0), "invalid address");
+        require(_bp != address(0), "invalid address");
         BP = BPContract(_bp);
+    }
+
+    function updateClaimPool(address pool) public onlyRole(OWNER_ROLE) {
+        require(pool != address(0), "invalid address");
+        CLAIM_POOL = pool;
+    }
+
+    function updateDailyMint(uint amount) public onlyRole(OWNER_ROLE) {
+        require(amount > 0, "invalid amount");
+        DAILY_MINT = amount;
     }
 
     function _beforeTokenTransfer(address from, address to, uint amount) internal override(ERC20) {
